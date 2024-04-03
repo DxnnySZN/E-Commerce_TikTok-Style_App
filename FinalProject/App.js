a2abb39ba1f9a4a8f17473b5228ef9b590c4638f
DxnnySZN <yoondaniel2006@gmail.com>
2024-04-02 23:21:30 -0500

    finished FYP UX 4/2/24
    sometimes it takes too long for the user to receive a new product when either accepting or denying the current product

--- FinalProject/App.js
+++ FinalProject/App.js
@@ -8,13 +8,17 @@ import { Image } from 'react-native';
 import { ActivityIndicator } from 'react-native';
 import colors from './app/config/colors';
 import * as Font from "expo-font";
+import { useCallback } from 'react';
 
 const getFonts = () => Font.loadAsync({
   "lexend-regular": require("C:/ReactNative/E-Commerce_TikTok-Style_App/FinalProject/app/assets/fonts/Lexend-Regular.ttf"),
+  // "C:\Users\yoon1521\Documents\GitHub\E-Commerce_TikTok-Style_App\FinalProject\app\assets\fonts\Lexend-Regular.ttf"
   "lexend-semiBold": require("C:/ReactNative/E-Commerce_TikTok-Style_App/FinalProject/app/assets/fonts/Lexend-SemiBold.ttf")
+  // "C:\Users\yoon1521\Documents\GitHub\E-Commerce_TikTok-Style_App\FinalProject\app\assets\fonts\Lexend-SemiBold.ttf"
 });
 
 export default function App() {
+  // state variable to track whether custom fonts have been loaded
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   // hold product data
@@ -22,81 +26,94 @@ export default function App() {
   const [productTitle, setProductTitle] = useState(null);
   const [productPrice, setProductPrice] = useState(null);
 
+  const fetchNewProduct = useCallback(() => { // useCallback prevents fetching of new products during each render
+    // fetch product data only when fonts are loaded
+    if(fontsLoaded){
+      // DO NOT CLICK THIS LINK UNLESS YOU WANT TO WASTE CREDITS ($)
+      axios.get("https://api.bluecartapi.com/request?api_key=3A09B5D146A84189AE44C3A628164CF1&search_term=electronics&type=search")
+      .then(response => {
+        // get random index within range of search_results array
+        const randomIndex = Math.floor(Math.random() * response.data.search_results.length);
+
+        // search_results array contains all walmart electronics items
+        setProductImg(response.data.search_results[randomIndex].product.main_image);
+        setProductTitle(response.data.search_results[randomIndex].product.title);
+        setProductPrice("$" + response.data.search_results[randomIndex].offers.primary.price);
+      })
+      .catch(error => {
+        console.log("Error status:", error.response.status);
+        console.log("Error data:", error.response.data);
+        console.log("Error message:", error.message);
+      });
+    }
+  }, [fontsLoaded]); 
+
   useEffect(() => {
-    async function loadFonts(){
+    async function loadFonts() {
       await getFonts();
       setFontsLoaded(true);
     }
-    loadFonts();
+    loadFonts(); fetchNewProduct();
+  }, [fetchNewProduct]); 
 
-    // fetch product data only when fonts are loaded
-    if (fontsLoaded) {
-      // DO NOT CLICK THIS LINK UNLESS YOU WANT TO WASTE CREDITS ($)
-      axios.get("https://api.bluecartapi.com/request?api_key=3A09B5D146A84189AE44C3A628164CF1&search_term=electronics&type=search")
-        .then(response => {
-          // search_results array contains all walmart electronics items
-          setProductImg(response.data.search_results[0].product.main_image);
-          setProductTitle(response.data.search_results[0].product.title); 
-          setProductPrice("$" + response.data.search_results[0].offers.primary.price);
-        })
-        .catch(error => {
-          console.log("Error status:", error.response.status);
-          console.log("Error data:", error.response.data);
-          console.log("Error message:", error.message);
-        });
-    }
-  }, [fontsLoaded]); // run the effect whenever fontsLoaded changes
+  const handleAcceptProduct = () => {
+    fetchNewProduct();
+  };
 
-  if (!fontsLoaded) {
-    return <ActivityIndicator size="large" color={colors.taskbarContainerColor} />;
-  }
+  const handleDeclineProduct = () => {
+    fetchNewProduct();
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.searchButton}>
-        <AntDesign name="search1" size={35} color="black"
-         onPress={() => console.log("button pressed")} />
+    <View style = {styles.container}>
+      <View style = {styles.searchButton}>
+        <AntDesign name = "search1" size = {35} color = "black"
+         onPress = {() => console.log("button pressed")}/>
       </View>
 
-      <View style={styles.taskbarContainer}>
-        <View style={styles.homeButton}>
-          <Octicons name="home" size={40} color="black"
-           onPress={() => console.log("button pressed")} />
+      <View style = {styles.taskbarContainer}>
+        <View style = {styles.homeButton}>
+          <Octicons name = "home" size = {40} color = "black"
+           onPress = {() => console.log("button pressed")}/>
         </View>
-        <View style={styles.discoverButton}>
-          <Ionicons name="compass-outline" size={50} color="black"
-           onPress={() => console.log("button pressed")} />
+        <View style = {styles.discoverButton}>
+          <Ionicons name = "compass-outline" size = {50} color = "black"
+           onPress = {() => console.log("button pressed")}/>
         </View>
-        <View style={styles.cartButton}>
-          <MaterialCommunityIcons name="cart-outline" size={43} color="black"
-           onPress={() => console.log("button pressed")} />
+        <View style = {styles.cartButton}>
+          <MaterialCommunityIcons name = "cart-outline" size = {43} color = "black"
+           onPress = {() => console.log("button pressed")}/>
         </View>
-        <View style={styles.profileButton}>
-          <Octicons name="person" size={43} color="black"
-           onPress={() => console.log("button pressed")} />
+        <View style = {styles.profileButton}>
+          <Octicons name = "person" size = {43} color = "black"
+           onPress = {() => console.log("button pressed")}/>
         </View>
       </View>
 
-    <View style={styles.productListingContainer}>
-      {productImg && productTitle && productPrice ? (
-        <View style={styles.productItem}>
-          <Image source={{ uri: productImg }} style={styles.productImg} />
-          <Text style={styles.productTitle}>{productTitle}</Text>
-          <Text style={styles.productPrice}>{productPrice}</Text>
-          <View style={styles.productButtonsContainer}>
-            <View style={[styles.productAcceptButton, styles.buttonElevation]}>
-              <MaterialIcons name="add-shopping-cart" size={45} color="green" onPress={() => console.log("button pressed")} />
+    <View style = {styles.productListingContainer}>
+      {!fontsLoaded && (
+        <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          {/* ActivityIndicator component used to indicate loading state */}
+          <ActivityIndicator size = "large" color = {colors.taskbarContainerColor}/> 
+        </View>
+      )}
+      {productImg && productTitle && productPrice && (
+        <View style = {styles.productItem}>
+          <Image source = {{ uri: productImg }} style = {styles.productImg}/>
+          <Text style = {styles.productTitle}>{productTitle}</Text>
+          <Text style = {styles.productPrice}>{productPrice}</Text>
+          <View style = {styles.productButtonsContainer}>
+            <View style = {[styles.productAcceptButton, styles.buttonElevation]}>
+              <MaterialIcons name = "add-shopping-cart" size = {45} color = "green" onPress = {handleAcceptProduct}/>
             </View>
-            <View style={[styles.productDeclineButton, styles.buttonElevation]}>
-              <Ionicons name="trash-outline" size={45} color="red" onPress={() => console.log("button pressed")} />
+            <View style = {[styles.productDeclineButton, styles.buttonElevation]}>
+              <Ionicons name = "trash-outline" size = {45} color = "red" onPress = {handleDeclineProduct}/>
             </View>
           </View>
         </View>
-      ) : (
-        <ActivityIndicator size="large" color={colors.taskbarContainerColor} /> // show loading indicator if product data is not available
       )}
     </View>
-      <StatusBar style="auto" />
+      <StatusBar style = "auto"/>
     </View>
   );
 }
@@ -148,13 +165,13 @@ const styles = StyleSheet.create({
   },
   productTitle: {
     marginTop: 30, 
-    fontSize: 18, 
+    fontSize: 15, 
     fontFamily: "lexend-semiBold",
     textAlign: "center",
     color: colors.taskbarContainerColor, 
   },
   productPrice: {
-    marginTop: 30, 
+    marginTop: 20, 
     fontSize: 45, 
     fontFamily: "lexend-regular",
     textAlign: "center",
